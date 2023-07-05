ea088d5226f3650bf71e74d5e503ae16342f7a2b
tusharginotra <tusharginotra2000@gmail.com>
2023-07-05 12:46:36 +0530

    Added Auth0 authentication

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-v4wf2y61svb7tjab.us.auth0.com"
+    clientId="HuotFYoYx6m53pfZHM5clKbxJLedYCxG"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
