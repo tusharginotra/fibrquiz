f0ac2e46045b72972d762a6368f823205fa40bd2
tusharginotra <tusharginotra2000@gmail.com>
2023-07-08 16:01:55 +0530

    added the backend endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 
 const config = {
-    endpoint : "http://localhost:8082/api/v1"
+    // endpoint : "http://localhost:8082/api/v1"
+    endpoint : "https://fibrbackend.onrender.com/api/v1"
 }
 export default config
